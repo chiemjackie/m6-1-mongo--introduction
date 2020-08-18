1df2e96b3ffb68f2d9403db5df76707caf90899f
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-08-17 20:57:09 -0400

    Exercise 2.1

---
+++ exercises/exercise-2.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+async function createGreeting(req, res) {
+  try {
+    const client = await MongoClient(MONGO_URI, { useUnifiedTopology: true });
+
+    await client.connect();
+
+    const db = client.db("exercise-1");
+
+    const dbResponse = await db.collection("greetings").insertOne(req.body);
+
+    assert.equal(1, dbResponse.insertedCount);
+
+    client.close();
+
+    res.status(201).json({ status: 201, data: req.body });
+  } catch ({ message }) {
+    res.status(500).json({ status: 500, message });
+  }
+}
+
+module.exports = { createGreeting };
